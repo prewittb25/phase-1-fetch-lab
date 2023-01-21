b920697258e663a76f3f56c8475b2fc3edead2bf
Barry <Prewittb25@gmail.com>
2023-01-20 20:51:58 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json))
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +19,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
+
